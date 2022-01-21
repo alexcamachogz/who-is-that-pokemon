18499bf8bbd38bdc3004d53f154f7fd0491ea579
Alex Camacho <camacho.ale94@gmail.com>
2022-01-21 00:44:19 -0600

    Creating Options and Picture component and making their styles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" class="fade-in">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <pokemon-page msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PokemonPage from './pages/PokemonPage'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PokemonPage
   }
 }
 </script>
