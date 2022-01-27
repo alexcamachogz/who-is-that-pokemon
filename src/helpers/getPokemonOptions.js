d9421b840be29d77b00f0c9580fa439e3fc07577
Alex Camacho <camacho.ale94@gmail.com>
2022-01-26 21:19:18 -0600

    Calling pokemon api to get a list of pokemons

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -1,15 +1,32 @@
+import pokeApi from '../pages/api/pokemonApi'
+
 const getPokemons = () => {
-  const pokemonsArr = Array.from(Array(650))
-  return pokemonsArr.map((_, index) => index + 1)
+  const pokeArray = Array.from(Array(650))
+  return pokeArray.map((_, index) => index + 1)
 }
 
-const getPokemonsNames = ([pokeOptA, pokeOptB, pokeOptC, pokeOptD] = []) => {
-  console.log(pokeOptA, pokeOptB, pokeOptC, pokeOptD)
+const getPokemonsNames = async ([pokeOptA, pokeOptB, pokeOptC, pokeOptD] = []) => {
+  const promisePokeArray = [
+    pokeApi.get(`/${pokeOptA}`),
+    pokeApi.get(`/${pokeOptB}`),
+    pokeApi.get(`/${pokeOptC}`),
+    pokeApi.get(`/${pokeOptD}`)
+  ]
+
+  const response = await Promise.all(promisePokeArray)
+
+  return response.map(poke => {
+    return {
+      name: poke.data.name,
+      id: poke.data.id
+    }
+  })
 }
 
-const getPokemonOptions = () => {
+const getPokemonOptions = async () => {
   const mixedPokemons = getPokemons().sort(() => Math.random() - 0.5)
-  getPokemonsNames(mixedPokemons.splice(0, 4))
+  const pokeNames = await getPokemonsNames(mixedPokemons.splice(0, 4))
+  return pokeNames
 }
 
 export default getPokemonOptions
